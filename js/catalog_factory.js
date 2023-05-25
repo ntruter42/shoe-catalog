a231c459b0e76fa7c2ca3757ae598501e207529c
ntruter42 <ntruter42@gmail.com>
2023-05-25 10:46:09 +0200

    Update index.html, style.css, catalog_dom.js, catalog_factory.js - added catagroies and guide comments

---
+++ js/catalog_factory.js
@@ -0,0 +1,26 @@
+function ShoeCatalog() {
+	// variable for shoe catalog list
+	// variable for shopping cart
+	
+	// function to add shoe to catalog
+
+	// function to remove shoe from catalog
+
+	// function to set entire catalog list
+
+	// function to add shoe to shopping cart
+
+	// function to remove shoe from shopping cart
+
+	// function to set entire shopping cart
+
+	// function to clear shopping cart
+
+	// function to get specific shoe
+
+	// function to filter shoe list
+
+	// function to sort shoe list
+
+	// return functions
+}
