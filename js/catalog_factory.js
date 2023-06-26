c5abe4fc7710b635b87c111f2023e3d1e6604066
ntruter42 <ntruter42@gmail.com>
2023-06-26 23:50:02 +0200

    Created function to addShoe to shoeList

--- js/catalog_factory.js
+++ js/catalog_factory.js
@@ -1,8 +1,43 @@
 function ShoeCatalog() {
 	// variable for shoe catalog list
+	let shoeList = {
+		'1000': {
+			'brand': "Under Armour",
+			'title': "Assert 9",
+			'type': "Running Shoe",
+			'sizeColorQuantity': {
+				'6,white': 9,
+				'6,black': 13,
+				'6,red': 3,
+				'7,white': 11,
+				'9,white': 7,
+				'9,black': 5,
+			}
+		},
+	};
 	// variable for shopping cart
-	
+
 	// function to add shoe to catalog
+	function addShoe(brand, model, type, sizeColorQuantity, price, picture) {
+		const shoe = {
+			brand: brand,
+			model: model,
+			type: type,
+			sizeColorQuantity: sizeColorQuantity,
+			price: price,
+			picture: picture
+		};
+
+		shoeList[generateShoeID()] = shoe;
+	}
+
+	function generateShoeID() {
+		const shoeIDList = Object.keys(shoeList);
+		if (shoeIDList.length > 0) {
+			return parseInt(shoeIDList[shoeIDList.length - 1]) + 1;
+		}
+		return 1000;
+	}
 
 	// function to remove shoe from catalog
 
@@ -23,4 +58,8 @@ function ShoeCatalog() {
 	// function to sort shoe list
 
 	// return functions
+	return {
+		addShoe,
+		generateShoeID,
+	}
 }
